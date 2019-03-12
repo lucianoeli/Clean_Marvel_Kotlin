e7037e89e30bfdaf4de0cb5f3253c46305593eb1
lucho eli <luciano.elissondo@globant.com>
2019-03-12 18:14:50 -0300

    characterPresenterTest

--- app/src/test/java/com/puzzlebench/clean_marvel_kotlin/presentation/mvp/CharacterPresenterTest.kt
+++ app/src/test/java/com/puzzlebench/clean_marvel_kotlin/presentation/mvp/CharacterPresenterTest.kt
@@ -1,5 +1,8 @@
 package com.puzzlebench.clean_marvel_kotlin.presentation.mvp
 
+import android.support.design.widget.FloatingActionButton
+import android.widget.Button
+import com.puzzlebench.clean_marvel_kotlin.data.local.SaveCharactersLocalImpl
 import com.puzzlebench.clean_marvel_kotlin.data.service.CharacterServicesImpl
 import com.puzzlebench.clean_marvel_kotlin.domain.model.Character
 import com.puzzlebench.clean_marvel_kotlin.domain.usecase.GetCharacterServiceUseCase
@@ -7,10 +10,14 @@ import com.puzzlebench.clean_marvel_kotlin.domain.usecase.SaveLocalCharactersUse
 import com.puzzlebench.clean_marvel_kotlin.mocks.factory.CharactersFactory
 import com.puzzlebench.clean_marvel_kotlin.presentation.mvp.presenter.CharacterPresenter
 import com.puzzlebench.clean_marvel_kotlin.presentation.mvp.view.CharacterView
+import io.reactivex.Completable
 import io.reactivex.Observable
+import io.reactivex.Single
 import io.reactivex.android.plugins.RxAndroidPlugins
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.schedulers.Schedulers
+import kotlinx.android.synthetic.main.activity_main.refreshFloatingButton
+import org.junit.Assert
 import org.junit.Before
 import org.junit.Ignore
 import org.junit.Test
@@ -21,25 +28,38 @@ import org.mockito.Mockito.verify
 //TODO fix on second iteration
 // error: However, there was exactly 1 interaction with this mock:
 class CharacterPresenterTest {
-
     private var view = mock(CharacterView::class.java)
     private var characterServiceImp = mock(CharacterServicesImpl::class.java)
-
+    private var saveCharactersLocalImpl = mock(SaveCharactersLocalImpl::class.java)
     private lateinit var getCharacterServiceUseCase: GetCharacterServiceUseCase
     private lateinit var saveLocalCharactersUseCase: SaveLocalCharactersUseCase
+    private lateinit var characterPresenter: CharacterPresenter
+
+    private lateinit var mButton: FloatingActionButton
 
     @Before
     fun setUp() {
+
         RxAndroidPlugins.setInitMainThreadSchedulerHandler { scheduler -> Schedulers.trampoline() }
         getCharacterServiceUseCase = GetCharacterServiceUseCase(characterServiceImp)
+        saveLocalCharactersUseCase = SaveLocalCharactersUseCase(saveCharactersLocalImpl)
         val subscriptions = mock(CompositeDisposable::class.java)
         characterPresenter = CharacterPresenter(view, getCharacterServiceUseCase, saveLocalCharactersUseCase, subscriptions)
+
+        view.init()
+        mButton = view.activityRef.get()?.refreshFloatingButton
+
+
+
     }
 
     @Ignore
     fun reposeWithError() {
+        val itemsCharecters = CharactersFactory.getMockCharacter()
+        val observable = Observable.just(itemsCharecters)
+        Mockito.`when`(getCharacterServiceUseCase.invoke()).thenReturn(observable)
         Mockito.`when`(getCharacterServiceUseCase.invoke()).thenReturn(Observable.error(Exception("")))
-        characterPresenter.init()
+
         verify(view).init()
         verify(characterServiceImp).getCharacters()
         verify(view).hideLoading()
@@ -53,11 +73,11 @@ class CharacterPresenterTest {
         val observable = Observable.just(itemsCharecters)
         Mockito.`when`(getCharacterServiceUseCase.invoke()).thenReturn(observable)
         characterPresenter.init()
+        view.activityRef.get()?.runOnUiThread(Runnable { mButton.requestFocus() })
+        mButton.performClick()
         verify(view).init()
         verify(characterServiceImp).getCharacters()
-        verify(view).hideLoading()
         verify(view).showCharacters(itemsCharecters)
-
     }
 
     @Test
@@ -68,7 +88,6 @@ class CharacterPresenterTest {
         characterPresenter.init()
         verify(view).init()
         verify(characterServiceImp).getCharacters()
-
     }
 
 }
